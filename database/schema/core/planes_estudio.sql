f1531910549313c3e1e29d82bbf0040423bfab90
Guillermo Staudt Ossa <gstaudt@inacap.cl>
2026-01-16 17:46:57 -0300

    Subido desde la Inacap a las 17:46 el 16-01

--- database/schema/core/planes_estudio.sql
+++ database/schema/core/planes_estudio.sql
@@ -1,7 +1,15 @@
 CREATE_TABLE Planes_Estudio (
     id_plan_estudio INT PRIMARY KEY AUTO_INCREMENT,
+    codigo_plan_estudio VARCHAR(20) NOT NULL UNIQUE,
     nombre_plan_estudio VARCHAR(200) NOT NULL UNIQUE,
+    año_vigencia INT NOT NULL,
+    semestre_vigencia INT NOT NULL,
+    estado ENUM("VIGENTE", "INACTIVO") NOT NULL,
+    total_creditos INT NOT NULL,
     descripcion TEXT,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE,
+
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    fecha_modificacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 )
